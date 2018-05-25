74dcd22ea40849b0d58166efdced56dd8f1ba677
Andy Ingram <andy@andrewingram.net>
2018-05-25 18:14:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ def parse_requirements(filename):
     lineiter = (line.strip() for line in open(filename))
     return [line for line in lineiter if line and not line.startswith("#")]
 
-install_reqs = parse_requirements('requirements.txt')
-reqs = [str(ir.req) for ir in install_reqs]
-
 setup(
     name='django-google-address',
     version='1.1.0',
@@ -22,5 +19,5 @@ setup(
     description='',
     long_description=open('README.rst', encoding='utf-8').read(),
     zip_safe=False,
-    install_requires=reqs
+    install_requires=parse_requirements('requirements.txt')
 )
