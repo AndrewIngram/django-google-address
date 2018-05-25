d8df0242fc67a2117ca68b98579753449c4f4bc0
Andy Ingram <andy@andrewingram.net>
2018-05-25 18:11:54 +0100

    remove need for pip.req

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
-install_reqs = parse_requirements('requirements.txt', session=False)
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+install_reqs = parse_requirements('requirements.txt')
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
